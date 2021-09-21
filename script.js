7db295f97f440a7026c879fbd62abf5e0b3cdae2
indhu-github <indhusworkmail@gmail.com>
2021-09-21 20:09:33 +0530

    dynamic year

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
 
 const currentYear = new Date().getFullYear();
+year.innerText = currentYear + 1;
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
 function updateCountdown() {

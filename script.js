f7b12289c26bc7e47192ad3f5cf4d7ecb6a76535
indhu-github <indhusworkmail@gmail.com>
2021-09-21 20:42:21 +0530

    loading spinner

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
 const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
 year.innerText = currentYear + 1;
@@ -12,16 +13,23 @@ const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 function updateCountdown() {
   const currentTime = new Date();
   const diffInSeconds = newYearTime - currentTime;
-  console.log(diffInSeconds);
+
   const d = Math.floor(diffInSeconds / 1000 / 60 / 60 / 24);
   const h = Math.floor((diffInSeconds / 1000 / 60 / 60) % 24);
   const m = Math.floor((diffInSeconds / 1000 / 60) % 60);
   const s = Math.floor(diffInSeconds / 1000) % 60;
 
+  //Add values to DOM
   days.innerHTML = d;
   hours.innerHTML = h < 10 ? "0" + h : h;
   minutes.innerHTML = m < 10 ? "0" + m : m;
   seconds.innerHTML = s < 10 ? "0" + s : s;
 }
 
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
+
+//Run every second
 setInterval(updateCountdown, 1000);
